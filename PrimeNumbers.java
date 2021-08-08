a5b6e77d33a13e0b4cc7e1f9caa65d80ea65bf8c
RamyaKrishnan004 <77857560+RamyaKrishnan004@users.noreply.github.com>
2021-08-08 10:38:47 +0530

    Add files via upload

---
+++ PrimeNumbers.java
@@ -0,0 +1,21 @@
+package day1;
+
+public class PrimeNumbers {
+	public static void main(String[] args) {
+		int num = 35;
+	    boolean flag = false;
+	    for (int i = 2; i <= num / 2; ++i) {
+	      // condition for non prime number
+	      if (num % i == 0) {
+	        flag = true;
+	        break;
+	      }
+	    }
+
+	    if (!flag)
+	      System.out.println(num + " is a prime number.");
+	    else
+	      System.out.println(num + " is not a prime number.");
+	  }
+	}
+
